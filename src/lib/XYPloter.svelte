e347582e58caf1923eadfd66f9b858cef5eb9b6e
Teesitin <teesitin.russell@gmail.com>
2024-03-21 21:25:49 -0400

    for freaking sake... tailwind rules

--- src/lib/XYPloter.svelte
+++ src/lib/XYPloter.svelte
@@ -1,55 +1,25 @@
 <script lang="ts">
-    import { onMount, onDestroy } from 'svelte';
+    import { questions } from '$lib/data';
     import { answerStore } from '$lib/answerStore';
+    import { Heading, P } from 'flowbite-svelte';
+	import { onDestroy, onMount } from 'svelte';
 
     let scores = { totalOptimist: 150, totalInnovator: 150 };
     answerStore.currentScores.subscribe(value => {
         scores = value || scores;
-        // Use the safe function to ensure this runs only in the browser.
-        safelyUpdateDotPosition();
     });
 
-    let dotX: number, dotY: number;
-    let imageElement: HTMLImageElement;
-
-    function updateDotPosition() {
-        if (!imageElement) return;
-        
-        // Ensure this logic runs after the browser has had a chance to paint.
-        const imageWidth = imageElement.offsetWidth;
-        const imageHeight = imageElement.offsetHeight;
-
-        const scaleFactor = imageWidth / 768; // Assuming 768 is the original design width.
-        dotX = (imageWidth / 2) + (scores.totalOptimist * 1.30 * scaleFactor);
-        dotY = (imageHeight / 2) - (scores.totalInnovator * 1.30 * scaleFactor);
-    }
-
-    function safelyUpdateDotPosition() {
-        if (typeof window !== "undefined") {
-            requestAnimationFrame(updateDotPosition);
-        } else {
-            console.log("requestAnimationFrame is not available in the current environment.");
-        }
-    }
-
-    onMount(() => {
-        // Use the safe function to ensure resize updates only occur in the browser.
-        const handleResize = () => safelyUpdateDotPosition();
-
-        window.addEventListener('resize', handleResize);
-
-        onDestroy(() => {
-            window.removeEventListener('resize', handleResize);
-        });
-    });
+    let dotX = 384 + (scores.totalOptimist * 1.30);
+    let dotY = 384 + (scores.totalInnovator * -1.30);
 </script>
 
+<div class="text-center mx-2 md:max-w-3xl md:mx-auto relative border-2">
+    <img src="/Ai-Test.png" alt="ai-xy-plot" class="w-full h-auto">
 
-
-<div class="text-center mx-2 md:max-w-3xl md:mx-auto relative">
-    <img src="/Ai-Test.png" alt="ai-xy-plot" class="w-full h-auto" bind:this={imageElement}>
-
-    <svg class="absolute top-0 left-0" style="width: 100%; height: 100%;">
+    <svg
+      class="absolute top-0 left-0 w-full h-auto max-w-[calc(100vw - 4rem)] max-h-[calc(100vw - 4rem)]"
+      viewBox="0 0 768 768"
+    >
         <circle cx={dotX} cy={dotY} r="5" fill="orange" stroke="black" />
     </svg>
 </div>
