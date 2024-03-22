5eb8722d0ca9ca6b713b4510988a1cae34158363
Teesitin <teesitin.russell@gmail.com>
2024-03-21 21:47:02 -0400

    google ads

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8185652632273055"
+    crossorigin="anonymous">
+  </script>
+</svelte:head>
+
 
 <div class="bg-gray-100 h-screen w-screen fixed top-0 left-0 z-[-999] dark:bg-gray-700">
 </div>
