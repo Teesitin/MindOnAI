5a93270e428cc0d03cc6780e3bca9ed7f25dc7c6
Teesitin <teesitin.russell@gmail.com>
2024-04-17 19:27:23 -0400

    les go

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -25,51 +25,48 @@ export const questions: Question[] = [
     { index: 101, question: "AI systems should make important decisions in healthcare.", optimist: 2, innovator: 5, subject: "Healthcare" },
     { index: 102, question: "The use of AI in medicine will raise ethical concerns about patient consent.", optimist: -2, innovator: 2, subject: "Healthcare" },
     { index: 103, question: "AI will revolutionize healthcare by predicting diseases before they occur.", optimist: 5, innovator: 4, subject: "Healthcare" },
+
     { index: 104, question: "AI-driven medical diagnosis can improve healthcare accessibility, but we must address privacy concerns in patient data handling.", optimist: 1, innovator: -3, subject: "Healthcare" },
-    
-    { index: 105, question: "AI should prioritize patient privacy over predictive accuracy.", optimist: -3, innovator: 4, subject: "Healthcare" },
+    { index: 105, question: "AI should prioritize patient privacy over accuracy.", optimist: -3, innovator: 4, subject: "Healthcare" },
     { index: 106, question: "Innovations in AI healthcare must be accessible to all, not just the wealthy.", optimist: 5, innovator: -1, subject: "Healthcare" },
-    { index: 107, question: "The pace of AI innovation in healthcare is more important than immediate practical applications.", optimist: -2, innovator: 5, subject: "Healthcare" },
-    
+    { index: 107, question: "Governments must oversee the application of AI in healthcare, while simultaneously making it easy to use in hospitals.", optimist: 5, innovator: -4, subject: "Healthcare" },
 
     // Education
     { index: 201, question: "Education systems should include AI literacy as a core subject.", optimist: 3, innovator: 3, subject: "Education" },
     { index: 202, question: "AI will democratize access to education globally.", optimist: 4, innovator: 3, subject: "Education" },
-    { index: 203, question: "AI has the potential to revolutionize education, but we must ensure it doesn't replace human interaction in learning.", optimist: 0, innovator: -2, subject: "Education" },
-    { index: 204, question: "AI will enable more effective and personalized learning experiences.", optimist: 2, innovator: 4, subject: "Education" },
-    { index: 205, question: "Dependency on AI will weaken human cognitive abilities.", optimist: -1, innovator: -1, subject: "Education" },
+    { index: 203, question: "AI will enable more effective and personalized learning experiences.", optimist: 2, innovator: 4, subject: "Education" },
 
-    { index: 206, question: "AI's role in education should primarily support teachers, not replace them.", optimist: 4, innovator: -2, subject: "Education" },
-    { index: 207, question: "Investing in AI for personalized learning is crucial, even if it leads to short-term inequalities.", optimist: -1, innovator: 5, subject: "Education" },
-    { index: 208, question: "The most significant benefit of AI in education is enhancing human creativity.", optimist: 5, innovator: -3, subject: "Education" },
-    
+    { index: 204, question: "AI has the potential to revolutionize education, but we must ensure it doesn't replace human interaction in learning.", optimist: 5, innovator: -5, subject: "Education" },
+    { index: 205, question: "Dependency on AI will weaken human cognitive abilities.", optimist: -3, innovator: 0, subject: "Education" },
+    { index: 206, question: "AI's role in education should primarily support teachers, not replace them.", optimist: 4, innovator: -3, subject: "Education" },
+    { index: 207, question: "Investing in AI for personalized learning is crucial, even if it leads to short-term inequalities.", optimist: -5, innovator: 5, subject: "Education" },
+    { index: 208, question: "A teacher should be entitled to incorporate AI into their teaching practices, even if it raises privacy issues.", optimist: 0, innovator: 5, subject: "Education" },
 
     // Ethics and Morality
-    { index: 301, question: "Privacy concerns are secondary to the benefits AI can offer.", optimist: 2, innovator: -4, subject: "Ethics" },
-    { index: 302, question: "AI can fully replicate human creativity in art and music.", optimist: 4, innovator: 5, subject: "Ethics" },
-    { index: 303, question: "AI-driven personal assistants will improve quality of life.", optimist: 5, innovator: 4, subject: "Ethics" },
-    { index: 304, question: "The rise of AI will necessitate a universal basic income.", optimist: -2, innovator: 3, subject: "Ethics" },
-    { index: 305, question: "Overreliance on AI in decision-making processes will diminish human expertise.", optimist: -3, innovator: -1, subject: "Ethics" },
-    { index: 306, question: "AI in entertainment will create hyper-personalized content.", optimist: 4, innovator: 5, subject: "Ethics" },
-    { index: 307, question: "It is unethical to create AI that mimics human emotions.", optimist: -1, innovator: -5, subject: "Ethics" },
+    { index: 301, question: "AI can fully replicate human creativity in art and music.", optimist: 4, innovator: 5, subject: "Ethics" },
+    { index: 302, question: "AI-driven personal assistants will improve quality of life.", optimist: 5, innovator: 4, subject: "Ethics" },
+    { index: 303, question: "Overreliance on AI in decision-making processes will diminish human expertise.", optimist: -3, innovator: -1, subject: "Ethics" },
+    { index: 304, question: "AI in entertainment will create hyper-personalized content.", optimist: 4, innovator: 5, subject: "Ethics" },
+    { index: 305, question: "It is unethical to create AI that mimics human emotions.", optimist: -1, innovator: -5, subject: "Ethics" },
+    { index: 306, question: "The advantages of AI outweigh privacy concerns.", optimist: 4, innovator: 4, subject: "Ethics" },
 
-    { index: 308, question: "The ethical implications of AI should be considered more important than its innovation.", optimist: 3, innovator: -5, subject: "Ethics" },
-    { index: 309, question: "AI should always reflect human ethical standards, even at the cost of efficiency.", optimist: 4, innovator: -4, subject: "Ethics" },
-    { index: 310, question: "Rapid innovation in AI ethics is necessary to address emerging moral dilemmas.", optimist: -3, innovator: 5, subject: "Ethics" },
+
+    { index: 307, question: "The rise of AI will necessitate a universal basic income.", optimist: -5, innovator: 5, subject: "Ethics" },
+    { index: 308, question: "The ethical implications of AI should be considered more important than its innovation.", optimist: 2, innovator: -5, subject: "Ethics" },
+    { index: 309, question: "AI should always reflect human ethical standards, even at the cost of efficiency.", optimist: 5, innovator: -5, subject: "Ethics" },
     
 
     // Society
     { index: 401, question: "AI technology will lead to greater inequalities in society.", optimist: -5, innovator: -4, subject: "Society" },
-    { index: 402, question: "AI will lead to new forms of art and expression previously unimaginable.", optimist: 4, innovator: 5, subject: "Society" },
-    { index: 403, question: "Bias in AI algorithms will exacerbate social inequalities.", optimist: -5, innovator: -3, subject: "Society" },
-    { index: 404, question: "AI-generated fake news and media will become a major challenge for societies.", optimist: -4, innovator: 2, subject: "Society" },
-    { index: 405, question: "The environmental impact of training large AI models is a significant concern.", optimist: -3, innovator: -1, subject: "Society" },
-    { index: 406, question: "AI will further decrease birth rates.", optimist: -4, innovator: 0, subject: "Society" },
-    { index: 407, question: "AI can bridge the gap between different social classes by providing equal access to information and resources.", optimist: 5, innovator: 4, subject: "Society" },
+    { index: 402, question: "Bias in AI algorithms will exacerbate social inequalities.", optimist: -5, innovator: -3, subject: "Society" },
+    { index: 403, question: "The environmental impact of training large AI models is a significant concern.", optimist: -5, innovator: -4, subject: "Society" },
+    { index: 404, question: "AI can bridge the gap between different social classes by providing equal access to information and resources.", optimist: 5, innovator: 4, subject: "Society" },
+    { index: 405, question: "AI-generated fake news and media will become a major challenge for societies.", optimist: -4, innovator: -1, subject: "Society" },
 
-    { index: 408, question: "AI's impact on reducing societal inequalities is more crucial than advancing technology.", optimist: 5, innovator: -2, subject: "Society" },
-    { index: 409, question: "Social advancements through AI should not compromise community values.", optimist: 4, innovator: -3, subject: "Society" },
-    { index: 410, question: "Innovative AI solutions should be pursued even if they disrupt societal norms.", optimist: -4, innovator: 5, subject: "Society" },
+    { index: 406, question: "AI will further decrease birth rates.", optimist: -1, innovator: -4, subject: "Society" },
+    { index: 407, question: "AI's impact on reducing societal inequalities is more crucial than advancing technology.", optimist: 5, innovator: -5, subject: "Society" },
+    { index: 408, question: "Social advancements through AI should not compromise community values.", optimist: 4, innovator: -3, subject: "Society" },
+    { index: 409, question: "Innovative AI solutions should be pursued even if they disrupt societal norms.", optimist: 0, innovator: 5, subject: "Society" },
     
 
     // Technology
@@ -77,49 +74,33 @@ export const questions: Question[] = [
     { index: 502, question: "Autonomous vehicles pose no significant ethical concerns.", optimist: 3, innovator: 5, subject: "Technology" },
     { index: 503, question: "AI in agriculture will lead to more sustainable farming practices.", optimist: 5, innovator: 4, subject: "Technology" },
     { index: 504, question: "AI-enhanced cybersecurity will be critical in defending against complex cyber threats.", optimist: 5, innovator: 5, subject: "Technology" },
-    { index: 505, question: "Public trust in AI systems is critical for widespread adoption.", optimist: 3, innovator: 2, subject: "Technology" },
-    { index: 506, question: "Robotic automation will drastically reduce human labor in manufacturing.", optimist: -2, innovator: 4, subject: "Technology" },
+    { index: 505, question: "Public trust in AI systems is critical for widespread adoption.", optimist: -2, innovator: -3, subject: "Technology" },
+    { index: 506, question: "Robotic automation will drastically reduce human labor in manufacturing.", optimist: -2, innovator: -2, subject: "Technology" },
 
-    { index: 507, question: "The pursuit of cutting-edge AI technology should not overshadow its practical applications.", optimist: 3, innovator: -5, subject: "Technology" },
-    { index: 508, question: "Technological breakthroughs in AI are more important than their immediate usability.", optimist: -5, innovator: 5, subject: "Technology" },
-    { index: 509, question: "AI technology must be developed with a focus on sustainability and environmental impact.", optimist: 5, innovator: -1, subject: "Technology" },
+    { index: 507, question: "Technological breakthroughs in AI are more important than their usability.", optimist: 1, innovator: 5, subject: "Technology" },
+    { index: 508, question: "AI technology must be developed with a focus on sustainability and environmental impact.", optimist: 5, innovator: -5, subject: "Technology" },
     
 
     // Judicial
     { index: 601, question: "AI-processed video evidence should be considered valid in court to ensure justice.", optimist: 4, innovator: 5, subject: "Judicial" },
-    { index: 602, question: "Audio evidence enhanced by AI, even without video, should be admissible in court.", optimist: -2, innovator: -4, subject: "Judicial" },
-    { index: 603, question: "AI systems could serve as unbiased jurors or witnesses, improving the fairness of trials.", optimist: 3, innovator: 4, subject: "Judicial" },
-    { index: 604, question: "AI judges could be more impartial than human judges, enhancing the justice system.", optimist: 5, innovator: 4, subject: "Judicial" },
-    { index: 605, question: "Reliance on AI for judging could lead to better sentences than those from human judges.", optimist: -4, innovator: 5, subject: "Judicial" },
-    { index: 606, question: "If AI achieves sentience, it should be granted legal rights and held accountable under law, including facing severe penalties.", optimist: 3, innovator: 5, subject: "Judicial" },
-    { index: 607, question: "AI systems, if sentient, must be subject to human laws and ethical standards.", optimist: -5, innovator: -3, subject: "Judicial" },
-    { index: 608, question: "AI should be regulated internationally, not just nationally.", optimist: 1, innovator: 3, subject: "Judicial" },
+    { index: 602, question: "Audio evidence enhanced by AI, even without video, should be admissible in court.", optimist: 2, innovator: 2, subject: "Judicial" },
+    { index: 603, question: "AI systems could serve as unbiased jurors or witnesses, improving the fairness of trials.", optimist: 5, innovator: 4, subject: "Judicial" },
+    { index: 604, question: "AI judges could be more impartial than human judges, enhancing the justice system.", optimist: 5, innovator: 5, subject: "Judicial" },
+    { index: 606, question: "AI should be regulated internationally, not just nationally.", optimist: -2, innovator: -4, subject: "Judicial" },
+    { index: 607, question: "Legal frameworks for AI need to evolve as quickly as the technology itself.", optimist: -2, innovator: -5, subject: "Judicial" },
 
-    { index: 609, question: "AI in the judiciary should focus on eliminating bias, even if it slows down innovation.", optimist: 4, innovator: -4, subject: "Judicial" },
-    { index: 610, question: "The rapid implementation of AI in legal processes is essential, despite ethical concerns.", optimist: -3, innovator: 5, subject: "Judicial" },
-    { index: 611, question: "Legal frameworks for AI need to evolve as quickly as the technology itself.", optimist: -2, innovator: 5, subject: "Judicial" },
+    { index: 608, question: "If AI achieves sentience, it should be granted legal rights and held accountable under law, including facing severe penalties.", optimist: 5, innovator: -5, subject: "Judicial" },
+    { index: 609, question: "The rapid implementation of AI in legal processes is essential, despite ethical concerns.", optimist: -3, innovator: 5, subject: "Judicial" },
     
-
-    // Political
-    { index: 701, question: "Implementing AI in political decision-making must consider ethical implications to ensure fairness and prevent bias.", optimist: 2, innovator: 3, subject: "Political" },
-    { index: 702, question: "AI-driven decisions, especially in politics, need strict regulation to safeguard against biases.", optimist: -4, innovator: 3, subject: "Political" },
-    { index: 703, question: "AI can enhance democratic processes through transparency and accountability, but must be carefully managed.", optimist: 4, innovator: 5, subject: "Political" },
-    { index: 704, question: "International rather than national regulation of AI is crucial for addressing global challenges and ensuring ethical governance.", optimist: 1, innovator: 3, subject: "Political" },
-
-    { index: 705, question: "The role of AI in politics should primarily be transparency, even at innovation's expense.", optimist: 5, innovator: -3, subject: "Political" },
-    { index: 706, question: "Advancements in AI for political analysis and decision-making should be prioritized.", optimist: -1, innovator: 5, subject: "Political" },
-    { index: 707, question: "Ethical AI usage in politics is more important than its potential to reshape governance.", optimist: 4, innovator: -4, subject: "Political" },
-    
-
     // Business and Jobs
-    { index: 801, question: "A company must be held fully accountable for any damages caused by its AI systems.", optimist: 0, innovator: -3, subject: "Business" },
-    { index: 802, question: "A company should receive full credit for any successes achieved through its AI systems.", optimist: 0, innovator: 3, subject: "Business" },
-    { index: 803, question: "AI will significantly reduce employment opportunities.", optimist: -4, innovator: -3, subject: "Business" },
-    { index: 804, question: "AI technologies should be used to monitor employee productivity in the workplace.", optimist: 5, innovator: 5, subject: "Business" },
+    { index: 801, question: "AI will significantly reduce employment opportunities.", optimist: -4, innovator: -3, subject: "Business" },
+    { index: 802, question: "AI technologies should be used to monitor employee productivity in the workplace.", optimist: 1, innovator: 3, subject: "Business" },
+    { index: 803, question: "A company should have the right to monitor employee productivity with AI systems.", optimist: 3, innovator: 5, subject: "Business" },
+    { index: 804, question: "In business, the ethical use of AI should take precedence over competitive advantage.", optimist: -1, innovator: -3, subject: "Business" },
+    { index: 805, question: "Embracing AI in business requires a bold approach, prioritizing innovation over tradition.", optimist: 4, innovator: 5, subject: "Business" },
 
-    { index: 805, question: "In business, the ethical use of AI should take precedence over competitive advantage.", optimist: 5, innovator: -2, subject: "Business" },
-    { index: 806, question: "The drive for innovation should not compromise workers' rights and job security.", optimist: 4, innovator: -3, subject: "Business" },
-    { index: 807, question: "Embracing AI in business requires a bold approach, prioritizing innovation over tradition.", optimist: -4, innovator: 5, subject: "Business" },
+    { index: 806, question: "A company should receive full credit for any successes achieved through its AI systems.", optimist: 0, innovator: 4, subject: "Business" },
+    { index: 807, question: "A company must be held fully accountable for any damages caused by its AI systems.", optimist: 0, innovator: -4, subject: "Business" },
 ];
   
 export const Subjects = [
@@ -129,8 +110,7 @@ export const Subjects = [
     { name: "Society", color: "#bbf7d0" },    // green
     { name: "Technology", color: "#c7d2fe" }, // indigo
     { name: "Judicial", color: "#e9d5ff" },   // purple
-    { name: "Political", color: "#ffedd5" },  // orange
-    { name: "Business", color: "#e5e7eb" }    // gray
+    { name: "Business", color: "#ffedd5" }    // orange
 ];
 
 
